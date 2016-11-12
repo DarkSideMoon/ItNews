867a4f634265e45249472716036c8a48fb60f4ca
Romashchenko Pavel <acubens96@meta.ua>
2016-11-13 00:09:23 +0200

    Initialize app

---
+++ README.md
@@ -0,0 +1,17 @@
+##It News :cyclone:
+
+Simple ukranian it news parser.
+With simple wrapper on `Express` server -> Adrin server 
+
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
+[![forthebadge](http://forthebadge.com/images/badges/uses-js.svg)](http://forthebadge.com)
+
+######Requirements:
+ * `Make friends with Node JS`
+ * `Using Visual studio Code` - if you like visual studio code you can use it in app
+ * `Node.js command prompt` - if you using node cmd go to log.js file and change path to file
+ 
+######Libs: 
+ * `Node-schedule`
+
+**Author:** Romashchenko Pavel 
---
+++ external/install.bat
@@ -0,0 +1,39 @@
+@echo Node JS Version
+node --version
+
+@echo NMP Version 
+npm --version
+
+@echo Create dir with app 
+pause
+
+mkdir myapp
+cd adrin
+
+npm init 
+
+
+@echo Install Server lib
+pause
+npm install express --save
+
+@echo Install all libs 
+pause
+npm install morgan --save
+npm install winston --save
+npm install serve-favicon --save
+npm install cookie-parser --save
+npm install body-parser --save
+npm install jade --save
+npm install cron --save
+npm install ejs --save
+npm install nodemailer --save
+npm install nodemailer-smtp-transport --save
+npm install xoauth2 --save
+npm install async --save
+npm install googleapis --save
+npm install cheerio --save 
+npm install request --save 
+npm install wundergroundnode -- save
+
+
